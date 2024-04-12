03f3a61615e1fb394ff7b74166585c3ea024290d
Kurt Taylor <ktaylor@deltahat.com>
2024-04-12 12:37:32 +0100

    add additional matches to test

--- R/2-maic-analyses.R
+++ R/2-maic-analyses.R
@@ -74,7 +74,7 @@ source("R/functions/maic/f_multi_maic_package.R")
 
 # VERSION OF RESULTS
 
-version <- "v0-4"
+version <- "v0-5"
 dir.create(file.path(results_folder, version),
            showWarnings = FALSE, recursive = TRUE)
 
@@ -166,18 +166,22 @@ match_maic_4 <- c("mean_characteristic_1",
                   "mean_characteristic_3",
                   "mean_characteristic_4")
 
-# match_maic_5 <- c("mean_characteristic_1",
-#                   "mean_characteristic_2",
-#                   "mean_characteristic_3",
-#                   "mean_characteristic_4",
-#                   "mean_characteristic_5")
-# 
-# match_maic_6 <- c("mean_characteristic_1",
-#                   "mean_characteristic_2",
-#                   "mean_characteristic_3",
-#                   "mean_characteristic_4",
-#                   "mean_characteristic_5",
-#                   "mean_characteristic_6")
+match_maic_5 <- c("mean_characteristic_1",
+                  "mean_characteristic_2",
+                  "mean_characteristic_3",
+                  "mean_characteristic_4",
+                  "proportion_characteristic_1_yes")
+
+match_maic_6 <- c("mean_characteristic_1",
+                  "mean_characteristic_2",
+                  "mean_characteristic_3",
+                  "mean_characteristic_4",
+                  "proportion_characteristic_1_yes",
+                  "median_characteristic_1")
+
+match_maic_7 <- c("proportion_characteristic_1_yes")
+
+match_maic_8 <- c("proportion_characteristic_2_yes")
 
 # Get all variables in the environment that start with "match_"
 match_vectors <- ls(pattern = "^match_maic")
